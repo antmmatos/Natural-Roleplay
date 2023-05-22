f0af579ef5f84b94e0376201f4aded945f5b9daa
António Matos <antonio.martim.matos@gmail.com>
2023-05-22 09:41:43 +0100

    Clothing part I

---
+++ txData/Base/resources/[Natural]/NaturalClothing/__resource.lua
@@ -0,0 +1,9 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+files {
+    'mp_f_freemode_01_kit_tropa_grota_as.meta',
+    'mp_m_freemode_01_kit_tropa_grota_as.meta'
+}
+ 
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_kit_tropa_grota_as.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_kit_tropa_grota_as.meta'
